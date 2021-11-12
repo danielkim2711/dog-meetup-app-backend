fdeedc7c0e35377d755461dbf60db89ea336feaf
Daniel Kim <danielkim2711@gmail.com>
2021-11-12 14:19:28 +1300

    Define models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+
+
+class Dog(models.Model):
+    GENDER_MALE = 'M'
+    GENDER_FEMALE = 'F'
+
+    GENDER_CHOICES = [
+        (GENDER_MALE, 'Male'),
+        (GENDER_FEMALE, 'Female'),
+    ]
+
+    dog_name = models.CharField(max_length=100)
+    dog_breed = models.CharField(max_length=100)
+    dog_gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from rest_framework.response import Response
+
 # Create your views here.
+
+
+def test(request):
+    return Response('Hello World')
