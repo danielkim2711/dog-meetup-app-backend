de46eef5837c680d568edc656e54db93c8911f53
Daniel Kim <danielkim2711@gmail.com>
2021-11-16 17:03:29 +1300

    Create password encryption and generate token

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,23 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.authtoken.views import Token
 from .models import Profile, Dog, Activity
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'username', 'password']
+
+        extra_kwargs = {'password': {
+            'write_only': True,
+            'required': True,
+        }}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
 
 
 class ProfileSerializer(serializers.ModelSerializer):
